37fc730db5b40e290c1efde423f33bc61c901184
Ryan Mattison <rmattison1@gmail.com>
2014-12-09 04:20:23 -0800

    List2009

--- UIPaginatedList/gen/com/ryanmattison/android/uipaginatedlistlibrary/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ryanmattison.android.uipaginatedlistlibrary;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
